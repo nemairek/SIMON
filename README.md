863a6ee4aca7498a98b1a9ce3f3435e99ee349ea
Knemaire <kannemaire@gmail.com>
2024-07-15 12:02:31 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#SIMON
+#SIMON 
--- app.js
+++ app.js
@@ -12,3 +12,24 @@
 
 /*----------- Event Listeners ----------*/
 
+let redButton = null;
+let yellowButton = null;
+let blueButton = null;
+let greenButton = null;
+
+let numberGen = [1,2,3,4]
+
+const randomNumber = numberGen[Math.floor(Math.random() * numberGen.length)]
+
+
+
+SIMON.addEventListener('click', (event) => {
+    const target = event.target;
+if (target.classList.contains('START')) {
+  
+    console.log(randomNumber)
+}
+
+
+
+});
